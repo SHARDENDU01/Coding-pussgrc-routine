8c3fc77c32ce0db7d634ea0ca1cba807adcfb9e2
SHARDENDU01 <SHARDENDU01@users.noreply.github.com>
2017-11-11 10:58:19 -0800

    call by reference using func.

---
+++ call by reference using func.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+    void change(int &);
+    int orig=10;
+    cout<<"The original value is:\t"<<orig<<endl;
+    change(orig);
+    cout<<"Value after change() is over:\t"<<orig<<endl;
+    return 0;
+}
+
+void change(int &orig)
+{
+    orig=20;
+    cout<<"Value of orig in function change() is:\t"<<orig<<endl;
+}
